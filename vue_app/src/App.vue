150e6d7e51b612c9daf48d75d291c1e091b38a3a
Gleb Ivanov <s22223@pjwstk.edu.pl>
2023-11-07 12:27:08 +0100

    test

---
+++ vue_app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <div class="login-container">
+      <h1>HOTERU ホテル</h1>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
