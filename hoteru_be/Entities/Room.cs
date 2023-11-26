04abec8f669beb371c102c0db8d301b39c3e640d
Gleb Ivanov <s22223@pjwstk.edu.pl>
2023-11-26 14:43:07 +0100

    added room entity validation

--- hoteru_be/Entities/Room.cs
+++ hoteru_be/Entities/Room.cs
@@ -9,9 +9,17 @@ namespace hoteru_be.Entities
         [Key]
         public int IdRoom { get; set; }
 
+        [Required]
+        [StringLength(50, ErrorMessage = "Room number must be at most 50 characters.")]
         public string Number { get; set; }
 
+        [Required]
+        [Range(1, 10, ErrorMessage = "Capacity must be between 1 and 10.")]
         public int Capacity { get; set; }
+
+        [Required]
+        [Range(1.0, 10000.0, ErrorMessage = "Price must be between 1 and 10000")]
+        [DataType(DataType.Currency)]
         public float Price { get; set; }
 
         public int IdRoomStatus { get; set; }
